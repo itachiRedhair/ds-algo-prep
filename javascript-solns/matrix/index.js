2d8edcd4b7ea86f14ab78277fa29c425cd257a3c
Akshay Milmile <akshay.milmile@tcs.com>
2021-10-19 16:30:43 +0530

    added javascript solution for spiral matrix

---
+++ javascript-solns/matrix/index.js
@@ -0,0 +1,75 @@
+// --- Directions
+// Write a function that accepts an integer N
+// and returns a NxN spiral matrix.
+// --- Examples
+//   matrix(2)
+//     [[1, 2],
+//     [4, 3]]
+//   matrix(3)
+//     [[1, 2, 3],
+//     [8, 9, 4],
+//     [7, 6, 5]]
+//  matrix(4)
+//     [[1,   2,  3, 4],
+//     [12, 13, 14, 5],
+//     [11, 16, 15, 6],
+//     [10,  9,  8, 7]]
+
+function matrix(n) {
+  const results = [];
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (!results[i]) {
+        results.push([]);
+      }
+      results[i][j] = 0;
+    }
+  }
+
+  let counter = 1;
+
+  let start_row = 0,
+    start_col = 0;
+
+  let end_row = n - 1,
+    end_col = n - 1;
+
+  debugger;
+
+  while (start_col <= end_col && start_row <= end_row) {
+    // Top row
+    for (let i = start_col; i <= end_col; i++) {
+      results[start_row][i] = counter;
+      counter++;
+    }
+    start_row++;
+
+    // right column
+    for (let i = start_row; i <= end_row; i++) {
+      results[i][end_col] = counter;
+      counter++;
+    }
+    end_col--;
+
+    // bottom row
+    for (let i = end_col; i >= start_col; i--) {
+      results[end_row][i] = counter;
+      counter++;
+    }
+    end_row--;
+
+    // left column
+    for (let i = end_row; i >= start_row; i--) {
+      results[i][start_col] = counter;
+      counter++;
+    }
+    start_col++;
+    debugger;
+  }
+
+  return results;
+}
+
+matrix(3);
+
+module.exports = matrix;
